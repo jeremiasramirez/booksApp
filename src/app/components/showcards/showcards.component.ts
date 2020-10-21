1c80b4187cf8ee303a5f4276596f7cea7fb466f6
Jeremias ramirez <jereramirez499@gmail.com>
2020-10-21 16:45:34 -0400

    estandarizando metodo watch para mostrar modal con dicho book clickeado

--- src/app/components/showcards/showcards.component.ts
+++ src/app/components/showcards/showcards.component.ts
@@ -1,21 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { timer } from 'rxjs';
+import { BookService } from 'src/app/services/service.book';
 
 @Component({
   selector: 'app-showcards',
   templateUrl: './showcards.component.html',
   styleUrls: ['./showcards.component.scss'],
+  providers: [
+    BookService
+  ]
 })
 export class ShowcardsComponent implements OnInit {
   @Input() private elements :any[] =[];
   private end:number=8;
 
-  constructor() { }
+  constructor(private service:BookService) { }
 
   ngOnInit() {}
 
+  private watch(data:{}) : void{
+    this.service.openToBook(data);
+  }
 
-   private more(event:any){
+  
+  private more(event:any){
     
     timer(500).subscribe(()=>{
       this.end+=6
